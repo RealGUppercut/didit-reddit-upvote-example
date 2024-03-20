c75323efbe31a2331097596779a1b8a9210b3f94
Joe Venton <joeventon@TechEd-0028.local>
2024-03-20 15:19:36 +0000

    envvvvvv

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,5 @@
 import { PostList } from "../components/PostList";
 
 export default async function Home() {
-  try {
-    return <PostList />;
-  } catch (error) {
-    console.error("NOT WORKING");
-  }
+  return <PostList />;
 }
